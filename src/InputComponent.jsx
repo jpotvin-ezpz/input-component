62a272dd9c52bdfe6f7bcbde68e05f55a5c20336
Pickles <pickles@pop-os.localdomain>
2021-03-24 12:04:33 -0400

    final commit

--- src/InputComponent.jsx
+++ src/InputComponent.jsx
@@ -1,18 +1,93 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 import './styles/index.css';
 
-const InputComponent = ({domId, placeholder='Placeholder', className,disabled, label='Label', id, helperText, startIcon}) => (
-  <div className={`inputComponent ${className}`} id={id}>
-    <label className={`classicLabel ${className}`} htmlFor={domId}>{label}
+const InputComponent = ({domId, placeholder='Placeholder', className,disabled, label='Label', id, helperText, startIcon, endIcon, initialText, multiline, row}) => {
+
+const [text, setText] = useState(initialText);
+
+if (startIcon) {
+  return (
+    <div className={`inputComponent ${className} startIcon`} id={id}>
+    <label className={`classicLabel ${className}`} htmlFor={domId}>
+      {label}
+      <span className='material-icons startIcon'>{startIcon}</span>
       <input 
+        type='text'
+        className={`classicInput startIcon ${className}`} 
+        placeholder={placeholder} 
+        disabled={disabled} 
+        id={domId} 
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        />
+      {helperText && <p>{helperText}</p>}
+    </label>
+  </div>
+  )
+}
+
+if (endIcon) {
+  return (
+  <div className={`inputComponent ${className} endIcon`} id={id}>
+    <label className={`classicLabel ${className}`} htmlFor={domId}>
+      {label}
+      <span className='material-icons endIcon'>{endIcon}</span>
+      <input 
+        type='text'
+        className={`classicInput endIcon ${className}`} 
+        placeholder={placeholder} 
+        disabled={disabled} 
+        id={domId} 
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        />
+      {helperText && <p>{helperText}</p>}
+    </label>
+  </div>
+  )
+ }
+
+if (multiline) { 
+  return (
+    <div className={`inputComponent ${className}`} id={id}>
+    <label className={`classicLabel ${className}`} htmlFor={domId}>
+      {label}
+      <textarea 
+        type='text'
+        className={`classicTextArea ${className}`} 
+        placeholder={placeholder} 
+        disabled={disabled} 
+        id={domId} 
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        rows={row}
+        />
+      {helperText && <p>{helperText}</p>}
+    </label>
+  </div>
+  )
+} else {
+    return (
+  <div className={`inputComponent ${className}`} id={id}>
+    <label className={`classicLabel ${className}`} htmlFor={domId}>
+      {label}
+      <input 
+        type='text'
         className={`classicInput ${className}`} 
         placeholder={placeholder} 
         disabled={disabled} 
         id={domId} 
+        value={text}
+        onChange={(e) => setText(e.target.value)}
         />
       {helperText && <p>{helperText}</p>}
     </label>
   </div>
 );
+}
+}
+
+
 
 export default InputComponent;
--- src/index.js
+++ src/index.js
@@ -17,8 +17,19 @@ ReactDOM.render(
       <InputComponent helperText='Some interesting text' className='error' label='<Input helperText="Some interesting text" />' />
     </div>
     <div className='showcase'>
-      <InputComponent startIcon='call' />
+      <InputComponent startIcon='call' label='<Input startIcon />' />
+      <InputComponent endIcon='call' label='<Input endIcon />' />
     </div>
+    <InputComponent label='<Input initialText="TEXT" />' initialText='TEXT' />
+    <div className="showcase">
+      <InputComponent label='<Input size="sm" />' className='sm'/>
+      <InputComponent label='<Input size="md" />' className='md'/>
+    </div>
+      <InputComponent label='<Input size="fullwidth" />' className='fullwidth'/>
+      <InputComponent label='<Input multiline row="4" />' multiline row='4'/>
+      <footer style={{color: 'darkgrey', margin:'0 2rem' }} >
+      created by jpotvin-ezpz - devChallenges.io
+      </footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
